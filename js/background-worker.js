1ae594e31d6364173da5176ccd6b8393fe07c5b8
Christian Szech <christian.szech@live.de>
2022-03-21 08:06:28 +0100

    Improved page opening

--- js/background-worker.js
+++ js/background-worker.js
@@ -1,54 +1,63 @@
 var openedTabId = null;
 var openedWindowId = null;
-var isPopupOpen = false;
-var isNavigationInProcess = false;
+var notificationTabIds = {}
 
 const handleUrl = "https://www.bahn.de/";
 const closeWarningUrl = "https://www.bahn.de/angebot";
 
-async function openUrlAsync(url) {
-    if (!isPopupOpen) {
+async function openUrlAsync(url, windowId) {
+    if (windowId == null) {
         await openNewWindowAsync(url);
     }
     else {
         navigateToUrl(openedTabId, url);
     }
-    chrome.windows.update(openedWindowId, { focused: true });
+}
+
+function focusWindow(windowId){
+    chrome.windows.get(windowId)
+        .then(window => {
+            chrome.windows.update(window.id, { focused: true });
+        });
+    
 }
 
 function isEmpty(str) {
-    return (!str || str.length === 0 );
+    return (!str || str.length === 0);
 }
 
 function isUrl(str) {
-    return (str && str.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/) );
+    return (str && str.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/));
 }
 
 function isNumber(str) {
-    return (str || str.match(/\d+/) );
+    return (str || str.match(/\d+/));
 }
 
 function navigateToUrl(tabId, newUrl) {
-    if( !tabId || !isUrl(newUrl) ){
+    if (!tabId || !isUrl(newUrl)) {
         return;
     }
-    chrome.tabs.update(tabId, { url: newUrl });
+    console.log(`Call the URL in the open tab with id ${tabId}.`);
+    chrome.tabs.update(tabId, {
+        url: newUrl
+    });
 }
 
 function resizeWindow(windowId, newWidth, newHeight) {
-    if(!windowId || !isNumber(newHeight) || !isNumber(newWidth) ){
+    if (!windowId || !isNumber(newHeight) || !isNumber(newWidth)) {
         return;
     }
     chrome.windows.update(windowId, { height: parseInt(newHeight), width: parseInt(newWidth) });
 }
 
-async function calcCenterWindow(width, height){
+async function calcCenterWindow(width, height) {
     const displayInfos = await chrome.system.display.getInfo();
     let point = {
-        left : 0,
-        top : 0
+        left: 0,
+        top: 0
     }
-    if( displayInfos.length > 0 ){
+    if (displayInfos.length > 0) {
         const displayBounds = displayInfos[0].bounds;
         point.left = ~~((displayBounds.width - width) / 2);
         point.top = ~~((displayBounds.height - height) / 2);
@@ -56,15 +65,16 @@ async function calcCenterWindow(width, height){
     return point;
 }
 
-async function centerWindow(windowId, width, height){
+async function centerWindow(windowId, width, height) {
     const point = await calcCenterWindow(width, height);
     chrome.windows.update(windowId, { left: point.left, top: point.top });
 }
 
 async function openNewWindowAsync(url) {
-    if(!isUrl(url) ){
+    if (!isUrl(url)) {
         return;
     }
+    console.log('Create new window and open URL.');
     const width = 1200;
     const height = 750;
     const point = await calcCenterWindow(width, height);
@@ -72,62 +82,68 @@ async function openNewWindowAsync(url) {
         focused: true,
         height: height,
         width: width,
-        left: point.left, 
+        left: point.left,
         top: point.top,
         type: "popup",
         url: url
     });
-    isPopupOpen = true;
     openedWindowId = window.id;
     openedTabId = window.tabs[0].id;
-    chrome.windows.onRemoved.addListener(onWindowClosed);
-    console.log('Finish navigation');
-    isNavigationInProcess = false;
+    console.log(`A new tab with id ${openedTabId} was created.`);
 }
 
 async function onTabUpdated(tabId, changeInfo, tab) {
-    console.log('Incoming: Tab='+tabId);
-    if(!isEmpty(changeInfo.url) && 
-        openedTabId != tabId && 
-        !isEmpty(handleUrl))
-    {
-        if(tab.url.includes(closeWarningUrl)){
-            ShowNotification();
+    if (openedTabId != tabId &&
+        !isEmpty(changeInfo.url) &&
+        !isEmpty(handleUrl)) {
+        console.log(`A new tab with id ${tabId} was detected.`);
+        if (tab.url.includes(closeWarningUrl)) {
+            showNotification(tabId);
         }
-        else if(tab.url.includes(handleUrl)){
-            isNavigationInProcess = true;
-            await openUrlAsync(tab.url);
-            console.log('Remove: Tab='+tabId);
-            chrome.tabs.remove(tabId);
+        else if (tab.url.includes(handleUrl)) {
+            await openUrlAsync(tab.url, openedWindowId);
+            focusWindow(openedWindowId);
+            removeTab(tabId);
         }
     }
 }
 
-function ShowNotification(){
-    chrome.notifications.create("Test", 
-    {
-        title : "Ein Titel",
-        iconUrl : "/icons/tabs-128x128.png",
-        type : "basic",
-        requireInteraction : true,
-        message : "Eine Nachricht",
-        buttons : [{title : "OK"}, {title : "Abbrechen"}]
-    },
-    onNotificationShown
-    );
+function removeTab(tabId) {
+    chrome.tabs.get(tabId)
+        .then(tab => {
+            console.log(`Remove tab with id ${tabId}.`);
+            chrome.tabs.remove(tab.id);
+        },
+            () => {
+                console.log(`There is no tab with id ${tabId}.`);
+            });
 }
 
-function onNotificationShown(notificationId) {
-    console.log("Test123");
+function showNotification(tabId) {
+    const notificationId = `TabsWarningMessage-${Date.now()}`;
+    chrome.notifications.create(notificationId,
+        {
+            type: "basic",
+            title: "Dokument noch in Bearbeitung",
+            message: "Möchten Sie die Bearbeitung des Dokuments abbrechen? Der aktuelle Bearbeitungsstand geht verloren.",
+            iconUrl: "/icons/tabs-128x128.png",
+            requireInteraction: true,
+            buttons: [{ title: "OK" }, { title: "Abbrechen" }]
+        });
+    notificationTabIds[notificationId] = tabId;
+}
+
+function onButtonClicked(notificationId, buttonIndex) {
+    
 }
 
 function onWindowClosed(closedWindowId) {
-    if(closedWindowId == openedWindowId){
-        isPopupOpen = false;
+    if (closedWindowId == openedWindowId) {
         openedTabId = null;
         openedWindowId = null;
     }
 }
 
-//chrome.webNavigation.onBeforeNavigate.addListener(onBeforeNavigate);
 chrome.tabs.onUpdated.addListener(onTabUpdated);
+chrome.notifications.onButtonClicked.addListener(onButtonClicked);
+chrome.windows.onRemoved.addListener(onWindowClosed);
