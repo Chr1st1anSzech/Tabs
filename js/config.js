9a0832bde6fff6003d395de27701d0b76b7d90e2
Christian S <christian.szech@live.de>
2022-03-23 09:52:13 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,14 +2,14 @@
  * Die URL, die in einem separaten Fenster geöffnet werden soll.
  * @const {string}
  */
- export const handleUrl = "http://lbrvctdm-e97.now-it.drv/rf-charta-ks/komposition/schreibenUebersicht.jsf";
+ export const handleUrl = "https://www.bahn.de";
 
 
  /**
   * Die URL, für die eine Benachrichtigung angezeigt werden soll.
   * @const {string}
   */
-  export const closeWarningUrl = "http://lbrvctdm-e97.now-it.drv/rf-charta-ks/komposition/metaDatenAdressat.jsf";
+  export const closeWarningUrl = "https://www.bahn.de/angebot/";
 
 
   /**
